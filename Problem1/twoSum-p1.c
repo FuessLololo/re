6004e0ef59520fde7c2eac10c7fabb145e6f5700
yuanhui.xia <yuanhui.xia@mujin.co.jp>
2025-06-03 16:02:45 +0900

    first commit

---
+++ Problem1/twoSum-p1.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
+{
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[j] == target - nums[i]) {
+                int* result = malloc(sizeof(int) * 2);
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    return malloc(sizeof(int) * 0);
+}
+
+int main() {
+    int nums[5] = { 7, 3, 5, 10, 22 };
+    int returnSize = 0;
+
+    int* twoSumResult = twoSum(nums, sizeof(nums) / sizeof(nums[0]), 32, &returnSize);
+
+    if (returnSize > 0){
+        for (int i = 0; i < returnSize; ++i){
+            printf("twoSumResult[%d]: %d\n", i, twoSumResult[i]);
+        }
+        free(twoSumResult);
+    }
+    else{
+        printf("No result\n");
+    }
+}
